d4c2a03b501d39a2e593aaa44b3633e808c5e9f3
aidanarz <aidan.ardhazizi@gmail.com>
2026-05-15 00:32:17 +0700

    add walpaper dan fix fitur group

--- IO-Chat-Backend/src/routes/chats.js
+++ IO-Chat-Backend/src/routes/chats.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getRecentChats, getMessages, createPrivateChat, createGroupChat, getChatMembers } from '../controllers/chatController.js';
+import { getRecentChats, getMessages, createPrivateChat, createGroupChat, getChatMembers, addGroupMembers } from '../controllers/chatController.js';
 import { authMiddleware } from '../middleware/auth.js';
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.use(authMiddleware);
 router.get('/', getRecentChats);
 router.post('/private', createPrivateChat);
 router.post('/group', createGroupChat);
+router.post('/group/:chatId/members', addGroupMembers);
 router.get('/:chatId/messages', getMessages);
 router.get('/:chatId/members', getChatMembers);
 
